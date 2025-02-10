e2837be45be71839c751553803183d3cbb25bcf4
dermatty <stephan@untergrabner.at>
2025-02-10 18:57:41 +0100

    python 3.13, poetry 2.0

--- guck4/__init__.py
+++ guck4/__init__.py
@@ -4,7 +4,7 @@ from importlib.metadata import version
 try:
     with open("pyproject.toml", mode="r") as config:
         toml_file = toml.load(config)
-    __version__ = toml_file["tool"]["poetry"]["version"]
+    __version__ = toml_file["project"]["version"]
     __appname__ = "guck" + __version__.split(".")[0]
     __appabbr__ = "g" + __version__.split(".")[0]
     __startmode__ = "dev"
